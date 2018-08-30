0ef5dbca4e62ba34d65dbd8e156b1a9ba3a49a82
sandeep45 <sandeep45@gmail.com>
2018-08-30 14:58:38 -0400

    added wrapper to access SimplyCompete

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'devise_token_auth'
 
 gem 'awesome_print'
 
+gem 'rest-client'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ lib/simply_compete_wrapper.rb
@@ -0,0 +1,45 @@
+# client = SimplyCompeteWrapper.new 'gallian83@hotmail.com', 'bttc', 1017
+# client.setup_session
+# client.download_csv
+
+class SimplyCompeteWrapper
+
+  AUTH_URL = 'https://usatt.simplycompete.com/j_spring_security_check'
+  RATINGS_URL_PREFIX = 'https://usatt.simplycompete.com/l/downloadRatings'
+
+  def initialize(username, password, league_id)
+    @username=username
+    @password=password
+    @league_id=league_id
+    @session_id = nil
+  end
+
+
+  def setup_session
+    form_data = {
+      j_username: @username,
+      j_password: @password,
+    }
+    RestClient::Request.execute(method: 'POST', url: AUTH_URL, payload: form_data) do |response|
+      puts response.headers
+      if response.headers[:location] =~ /authfail/
+        puts "login failure"
+      else
+        puts "login successful"
+        @session_id = response.cookies["JSESSIONID"]
+        puts @session_id
+      end
+    end
+  end
+
+  def download_csv
+    url = "#{RATINGS_URL_PREFIX}/#{@league_id}"
+    headers = {
+      cookies: {
+        JSESSIONID: @session_id
+      }
+    }
+    response = RestClient::Request.execute(method: 'POST', url: url, headers: headers)
+    puts response
+  end
+end
